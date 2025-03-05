3c0833b3cc2970afa8d373e21c91b8c959aa6960
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2025-03-05 13:27:00 +0300

    Redux
    I am doing my configurations on redux toolkit also I filtered my sidebar navigations.

--- task-management/src/App.js
+++ task-management/src/App.js
@@ -3,15 +3,21 @@ import {darkTheme} from "./theme/darktheme";
 import Navbar from "./page/navbar/Navbar";
 import Home from "./page/home/Home";
 import Auth from "./page/auth/Auth";
+import {useState} from 'react';
 import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
 
 
 function App() {
+    const user=true;
   return (
     <ThemeProvider theme={darkTheme}>
-        {/*<Navbar/>*/}
-        {/*<Home/>*/}
-        <Auth/>
+
+        {user? <div>
+            <Navbar/>
+            <Home/>
+        </div>: <Auth/>}
+
+
     </ThemeProvider>
   );
 }
---
+++ task-management/src/reduxtoolkit/Store.js
@@ -0,0 +1,15 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {thunk} from "redux-thunk";
+
+
+const rootReducer = combineReducers({
+
+})
+
+const store=configureStore({
+    reducer:rootReducer,
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(thunk)
+})
+
+export default store;
+
