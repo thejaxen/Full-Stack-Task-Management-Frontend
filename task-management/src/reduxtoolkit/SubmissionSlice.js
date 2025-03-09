bb48e80ca57e0eed5f265085099beba806cc1a81
Mert Duyar <86599460+thejaxen@users.noreply.github.com>
2025-03-09 20:26:15 +0300

    Last Slice
    I am done with the last slice which is the submission slice.

--- task-management/src/reduxtoolkit/SubmissionSlice.js
+++ task-management/src/reduxtoolkit/SubmissionSlice.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {api, setAuthHeader} from "../api/api";
 
 export const submitTask=createAsyncThunk("submissions/submitTask",
@@ -53,14 +53,15 @@ export const fetchSubmissionsByTaskId=createAsyncThunk("submissions/fetchSubmiss
 )
 
 export const acceptDeclineSubmission=createAsyncThunk("submissions/fetchSubmissionsByTaskId",
-    async(taskId)=>{
+    async({id,status})=>{
         setAuthHeader(localStorage.getItem("jwt",api))
 
         try{
-            const {data}=await api.get(`/api/submissions/task/${taskId}`,
+            const {data}=await api.put(
+                `/api/submissions/${id}?status=${status}`,
                 {}
             );
-            console.log("submitted tasks",data)
+            console.log("accept task",data)
             return data;
         }catch(error){
             console.log("catch ",error)
@@ -68,5 +69,49 @@ export const acceptDeclineSubmission=createAsyncThunk("submissions/fetchSubmissi
         }
     }
 )
+
+const submissionSlice = createSlice({
+    name:'submission',
+    initialState:{
+        submissions:[],
+        status:'idle',
+        error:null,
+    },
+    reducers: {},
+    extraReducers:(builder)=>{
+        builder
+            .addCase(submitTask.pending,(state)=>{
+                state.status='loading';
+            })
+            .addCase(submitTask.fulfilled,(state,action) => {
+                state.status='succeeded';
+                state.submissions.push(action.payload);
+            })
+            .addCase(submitTask.rejected, (state,action)=>{
+                state.status='failed';
+                state.error=action.error.message;
+            })
+            .addCase(fetchAllSubmissions.fulfilled, (state,action)=>{
+                state.status='succeeded';
+                state.submissions = action.payload;
+            })
+            .addCase(fetchAllSubmissions.rejected, (state,action)=>{
+                state.status='failed';
+                state.submissions = action.error.message;
+            })
+            .addCase(fetchSubmissionsByTaskId.fulfilled, (state,action)=>{
+                state.status='succeeded';
+                state.submissions=action.payload;
+            })
+            .addCase(acceptDeclineSubmission.fulfilled,(state,action)=>{
+                state.status='succeeded';
+                state.submissions=state.submissions.map((item)=>
+                    item.id !== action.payload.id ? item:action.payload
+                );
+            });
+    },
+});
+
+export default submissionSlice.reducer;
 
 
